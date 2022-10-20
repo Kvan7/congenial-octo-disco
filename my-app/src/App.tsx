798a9de501e67b3e375643d80ffbc311054e5ed8
Kvan7 <71402892+Kvan7@users.noreply.github.com>
2022-10-19 23:09:37 -0500

    dont know, just pushing

---
+++ my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div className="App">
+			<img src='https://cataas.com/cat' alt='https://cataas.com/cat' />
+		</div>
+	);
+}
+
+export default App;
