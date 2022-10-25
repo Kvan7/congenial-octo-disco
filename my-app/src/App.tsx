30bd6abf65038db6e8dcce27ac244d39f4364566
Kvan7 <71402892+Kvan7@users.noreply.github.com>
2022-10-25 06:37:07 -0500

    update add starter components

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import TitleBar from './components/HeaderComponent';
 
 function App() {
 	return (
-		<div className="App">
-			<img src='https://cataas.com/cat' alt='https://cataas.com/cat' />
+		<div className="App bg-gray-300 min-h-screen">
+			<div className='bg-gray-600'>
+				<TitleBar />
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
 		</div>
 	);
 }
---
+++ my-app/src/components/HeaderComponent.tsx
@@ -0,0 +1,15 @@
+import { Component, ReactNode } from "react";
+
+class TitleBar extends Component {
+
+
+	render(): ReactNode {
+		return (
+			<div className="h-96 relative">
+				<p className="font-mono text-5xl text-green-300 font-black absolute bottom-0 left-16">Tilte text 0</p>
+			</div>
+		);
+	}
+}
+
+export default TitleBar;
