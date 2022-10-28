f7f6999009b8a81a27fe2b89e9b8a18b3f7f4fdf
Kvan7 <71402892+Kvan7@users.noreply.github.com>
2022-10-28 15:16:39 -0500

    added random cat count

--- my-app/src/components/pages/Git.tsx
+++ my-app/src/components/pages/Git.tsx
@@ -7,20 +7,32 @@ type State = {}
 
 const carousel_child_css = 'w-full shrink-0 flex-grow';
 
+const sayss = ['hello', 'imsecond', 'last', 'nopeimhere', 'imthereallast', 'banana']
+
 export default class Git extends Component<Props, State> {
 	state = {}
 
 	render() {
+
+		var elms = [];
+		var count = Math.ceil(Math.random() * 4) + 2;
+		for (var i = 0; i < count; i++) {
+			elms.push(
+				<img src={"https://cataas.com/cat/says/" + sayss[i]} alt={i.toString()} className={carousel_child_css} />
+			);
+		}
+
 		return (
 			<div className='px-5'>
 				<div className='text-3xl'>
 					Git
 				</div>
 				<Carousel height="h-fit">
-					<img src="https://cataas.com/cat/says/hello" alt='1' className={carousel_child_css} />
-					<img src="https://cataas.com/cat/says/imNumer2" alt='2' className={carousel_child_css} />
+					{/* <img src="https://cataas.com/cat/says/hello" alt='1' className={carousel_child_css} /> */}
+					{/* <img src="https://cataas.com/cat/says/imNumer2" alt='2' className={carousel_child_css} /> */}
 					{/* <h1 className={carousel_child_css}>bottom_text</h1> */}
-					<img src="https://cataas.com/cat/says/Last" alt='3' className={carousel_child_css} />
+					{/* <img src="https://cataas.com/cat/says/Last" alt='3' className={carousel_child_css} /> */}
+					{elms}
 				</Carousel>
 			</div>
 		)
