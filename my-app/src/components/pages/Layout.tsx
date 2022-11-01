05eda97458516bd21e043b0599c176a8ccce6851
Kvan7 <71402892+Kvan7@users.noreply.github.com>
2022-11-01 11:57:14 -0500

    Made nav bar expand and unround when stuck

--- my-app/src/components/pages/Layout.tsx
+++ my-app/src/components/pages/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 
 type Props = {}
@@ -7,20 +7,41 @@ type State = {}
 
 const link_winds = 'px-10 inline-block max-w-xs w-1/12 text-center '
 
-export default class Layout extends Component<Props, State> {
-	state = {}
+const Layout = (props: Props) => {
+	const [isSticky, setIsSticky] = useState(false);
+	const ref = useRef<HTMLDivElement | null>(null);
+	// mount 
+	useEffect(() => {
+		const cachedRef = ref.current;
+		const observer = new IntersectionObserver(
+			([e]) => {
+				// setIsSticky(e.intersectionRatio >= 0.4316258351893096);
+				setIsSticky(e.isIntersecting);
+			},
+			{
+				threshold: [0.4316258351893096],
+				rootMargin: "16px",
+			}
+		);
+		if (cachedRef) {
+			observer.observe(cachedRef);
+			// unmount
+			return () => observer.unobserve(cachedRef);
+		}
 
-
-	render() {
-		return (
-			<div>
-				<nav className='bg-green-600 text-lg text-black font-semibold rounded-lg sticky top-0 my-4 mx-5 z-50'>
-					<NavLink to="/" end className={(navigationData) => link_winds + 'rounded-l-lg ' + (navigationData.isActive ? "bg-green-800" : "bg-green-600 hover:bg-green-700")}>Home</NavLink>
-					<NavLink to="/git" className={(navigationData) => link_winds + '' + (navigationData.isActive ? "bg-green-800" : "bg-green-600  hover:bg-green-700")}>Git</NavLink>
-					<NavLink to="/about" className={(navigationData) => link_winds + '' + (navigationData.isActive ? "bg-green-800" : "bg-green-600  hover:bg-green-700")}>About</NavLink>
-				</nav>
-				<Outlet />
-			</div>
-		)
-	}
+	}, [isSticky, ref])
+	const whenSticky = "";
+	const whenNotSticky = "rounded-lg top-0 my-4 mx-5";
+	return (
+		<div ref={ref} id="NavBarDivComponent" className="-top-px pt-px">
+			<nav className={(isSticky ? whenSticky : whenNotSticky) + ' bg-green-600 text-lg text-black font-semibold top-0 my-4 sticky z-50'}>
+				<NavLink to="/" end className={(navigationData) => link_winds + (isSticky ? " " : 'rounded-l-lg ') + (navigationData.isActive ? "bg-green-800" : "bg-green-600 hover:bg-green-700")}>Home</NavLink>
+				<NavLink to="/git" className={(navigationData) => link_winds + '' + (navigationData.isActive ? "bg-green-800" : "bg-green-600  hover:bg-green-700")}>Git</NavLink>
+				<NavLink to="/about" className={(navigationData) => link_winds + '' + (navigationData.isActive ? "bg-green-800" : "bg-green-600  hover:bg-green-700")}>About</NavLink>
+			</nav>
+			<Outlet />
+		</div>
+	);
 }
+
+export default Layout;
