a20f32b4effb3251418b575849422cdf74c8dabd
Kvan7 <71402892+Kvan7@users.noreply.github.com>
2022-11-11 12:45:03 -0600

    made nav bar functional component

--- my-app/src/components/NavBarComponent.tsx
+++ my-app/src/components/NavBarComponent.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import About from './pages/AboutPage';
 import FourZeroFour from './pages/FourZeroFour';
@@ -6,26 +6,38 @@ import GitPage from './pages/GitPage';
 import Home from './pages/HomePage';
 import Layout from './pages/Layout';
 
-type Props = {};
+// type Props = {};
 
-type State = {};
+// type State = {};
 
-export default class NavBarComponent extends Component<Props, State> {
-	constructor(props: Props) {
-		super(props);
-		this.state = {};
-	}
+export default function NavBarComponent() {
+	// const { pathname, hash, key } = useLocation();
 
-	render() {
-		return (
-			<Routes>
-				<Route path='/' element={<Layout />}>
-					<Route index element={<Home />} />
-					<Route path='git' element={<GitPage />} />
-					<Route path='about' element={<About />} />
-					<Route path='*' element={<FourZeroFour />} />
-				</Route>
-			</Routes>
-		);
-	}
+	// useEffect(() => {
+	// 	// if not a hash link, scroll to top
+	// 	if (hash === '') {
+	// 		window.scrollTo(0, 0);
+	// 	}
+	// 	// else scroll to id
+	// 	else {
+	// 		setTimeout(() => {
+	// 			const id = hash.replace('#', '');
+	// 			const element = document.getElementById(id);
+	// 			if (element) {
+	// 				element.scrollIntoView();
+	// 			}
+	// 		}, 0);
+	// 	}
+	// }, [pathname, hash, key]); // do this on route change
+
+	return (
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route index element={<Home />} />
+				<Route path='git' element={<GitPage />} />
+				<Route path='about' element={<About />} />
+				<Route path='*' element={<FourZeroFour />} />
+			</Route>
+		</Routes>
+	);
 }
